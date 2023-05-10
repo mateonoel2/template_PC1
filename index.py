713dca4aabff3e7b660c2833682106b7a015eeb3
mateonoel2 <mateo.noel@utec.edu.pe>
2023-05-09 23:17:22 -0500

    Avance

--- index.py
+++ index.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 from flask import Flask, jsonify,  request, render_template, redirect
 from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
@@ -21,6 +22,21 @@ class Player(db.Model):
 
     def __repr__(self):
         return f'<Player {self.firstname}>'
+    
+def test_connection():
+    with app.app_context():
+        db.create_all()
+        player_test = Player(firstname='john', lastname='doe')
+
+        db.session.add(player_test)
+        db.session.commit()
+
+        students = Player.query.all()
+
+        print(students)
+        print(jsonify(students))
+
+test_connection()
 
 @app.route('/')
 def menu():
@@ -38,39 +54,64 @@ def route_get_player(player_id):
 def route_add_player():
     firstname = request.form['firstname']
     lastname = request.form['lastname']
-    print(firstname)
-    print(lastname)
     player = Player(firstname=firstname, lastname=lastname)
     db.session.add(player)
     db.session.commit()
     return redirect('/')
 
-@app.route('/players/update',  methods = ['PUT'])
-def route_update_player():
-    player = request.get_json()
-    return update_player(player)
+@app.route('/players/delete', methods=['POST', 'DELETE'])
+def route_delete_player():
+    player_id = request.form['player_id']
+    player = Player.query.get(player_id)
+    if player is None:
+        error_message = f"No player with ID {player_id} found."
+        return render_template('menu.html', error_message=error_message)
+    db.session.delete(player)
+    db.session.commit()
+    return redirect('/')
 
-@app.route('/players/delete/<player_id>',  methods = ['DELETE'])
-def route_delete_player(player_id):
+@app.route('/players/delete/<player_id>', methods=['GET','DELETE'])
+def route_delete_player2(player_id):
     return delete_player(player_id)
 
+@app.route('/players/list', methods=['GET'])
+def route_list_players():
+    players = Player.query.all()
+    return render_template('menu.html', players=players)
 
 def get_players():
     players = Player.query.all()
     return jsonify(players)
 
-def get_player_by_id():
-    # Player.query.filter_by(id=3).first()
-    return 'TODO'
+def get_player_by_id(player_id):
+    player = Player.query.get(player_id)
+    return jsonify(player)
 
-def insert_player():
-    return 'TODO'
-
-def update_player():
-    return 'TODO'
+def insert_player(firstname, lastname):
+    new_player = Player(name=firstname, age=lastname)
+    db.session.add(new_player)
+    db.session.commit()
+    return new_player.id
 
 def delete_player(player_id):
-    player = Player.query.get_or_404(player_id)
-    # db.session.delete(player)
-    # db.session.commit()
-    return 'TODO'
+    player = Player.query.get(player_id)
+    if player is None:
+        return f"No player with ID {player_id} found."
+    db.session.delete(player)
+    db.session.commit()
+    return f"Player with ID {player_id} has been deleted."
+
+# @app.route('/players/update',  methods = ['PUT'])
+# def route_update_player():
+#     player = request.get_json()
+#     return update_player(player)
+
+# def update_player(player_id, name=None, age=None):
+#     player = Player.query.get(player_id)
+#     if player is not None:
+#         if name is not None:
+#             player.name = name
+#         if age is not None:
+#             player.age = age
+#         db.session.commit()
+#     return
