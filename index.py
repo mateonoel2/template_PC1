200bae066b8a7ac2b162ce46788279c7a9f8fcf8
mateonoel2 <mateo.noel@utec.edu.pe>
2023-05-17 15:11:41 -0500

    add changes

--- index.py
+++ index.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from flask import Flask, jsonify,  request, render_template, redirect, flash
+from flask import Flask, jsonify, request, render_template, redirect, flash
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
